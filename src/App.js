6895484edf86356d609bfa8a13bd0db250a35ae9
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2022-10-28 12:56:31 -0700

    display lists of images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [json, setJson] = useState("");
+  const textInputRef = useRef();
+  const [results, setResults] = useState([]);
+
+  const handleClick = async () => {
+    const res = await fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${textInputRef.current.value}&api_key=Er0LNZ7YEdLAuVXJn1FKj5qFgF10lodW`
+    );
+    const jsonRes = await res.json();
+    setJson(jsonRes);
+    setResults(jsonRes.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={textInputRef} />
+      <button onClick={handleClick}>submit</button>
+      <div class="container">
+        {results.map((result) => (
+          <div class="img-container">
+            <h2>Title: {result.title}</h2>
+            <div class="img-item">
+              <img src={`${result.images.original.url}`} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
 export default App;
+
+//Er0LNZ7YEdLAuVXJn1FKj5qFgF10lodW
+//api.giphy.com/v1/gifs/search?q=cheeseburgerz&api_key=Er0LNZ7YEdLAuVXJn1FKj5qFgF10lodW
+
+//display list of titles and images
